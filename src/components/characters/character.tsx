32b2f176cecad609b99805cb32171e5eb45d4e41
Floare Octavian <adifloare1969@gmail.com>
2026-07-14 12:21:29 +0300

    [gabit] Validate using zod

--- src/components/characters/character.tsx
+++ src/components/characters/character.tsx
@@ -1,6 +1,14 @@
 import { Link } from "react-router-dom"
 import type { Character as CharacterType } from "./hooks/useCharacters"
 
+type Something = {
+    caca?: string;
+}
+
+const somethingElse : Something = {
+    caca: "caca"
+}
+
 export const Character = ({ name, image, id }: CharacterType) => {
     return <div className="character">
         <Link to={`/character/${id}`}>
--- src/components/updateCharacter/index.tsx
+++ src/components/updateCharacter/index.tsx
@@ -1,26 +1,38 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
-type UpdateCharacterForm = {
-    name: string,
-    species: string,
-    gender: string,
-    origin: {
-        name: string,
-        dimension: string,
-    }
-}
+const UpdateCharacterSchema = z.object({
+    name: z.string()
+        .min(5, "Name must be at least 5 characters long")
+        .max(20, "Name must be at most 20 characters long")
+        .refine((name) => name?.[0] === name?.[0]?.toUpperCase(), "Name must start with an uppercase letter")
+    .nonoptional(),
+    species: z.string()
+    .min(1, "Species must be at least 1 character long")
+    .nonoptional(),
+    gender: z.union([z.undefined(), z.string()]),
+    origin: z.object({
+        name: z.string().nonoptional(),
+        dimension: z.string().nonoptional(),
+    }).nonoptional()
+})
+
+type UpdateForm = z.infer<typeof UpdateCharacterSchema>
 
 export const UpdateCharacter = () => {
     const {
         register,
         handleSubmit,
         formState: { errors },
-    } = useForm<UpdateCharacterForm>();
+    } = useForm<UpdateForm>({
+        resolver: zodResolver(UpdateCharacterSchema)
+    });
 
-    const [submitted, setSubmitted] = useState<UpdateCharacterForm | null>(null);
+    const [submitted, setSubmitted] = useState<UpdateForm | null>(null);
 
-    const onValid = (data: UpdateCharacterForm) => {
+    const onValid = (data: UpdateForm) => {
         setSubmitted(data)
         console.log("Form is valid");
     }
@@ -33,18 +45,21 @@ export const UpdateCharacter = () => {
             onSubmit={handleSubmit(onValid, onInvalid)}>
         <div>Form to update a character</div>
 
-
         <label>Character Name</label>
-        <input {...register("name", { required: true })} />
+        <input {...register("name")} />
 
 
-        {errors.name && <div style={{ color: "red" }}>Name is required</div>}
+        {errors.name && <div style={{ color: "red" }}>{errors.name.message}</div>}
 
         <label>Character Species</label>
         <input {...register("species")} />
 
+        {errors.species && <div style={{ color: "red" }}>{errors.species.message}</div>}
+
         <label>Character Gender</label>
-        <input {...register("gender")} />
+        <input {...register("gender"), { required: true }} />
+
+        {errors.gender && <div style={{ color: "red" }}>{errors.gender.message}</div>}
 
         <label>Character Origin (name)</label>
         <input {...register("origin.name")} />
@@ -53,15 +68,18 @@ export const UpdateCharacter = () => {
         <input {...register("origin.dimension")} />
 
         <label></label>
-        <button type="submit">Update</button>
+        <button 
+            type="submit"
+            style={{ width: "100px", height: "50px"}}
+            >Update</button>
 
     {submitted && (
     <div>
         <div>Name: {submitted.name}</div>
         <div>Species: {submitted.species}</div>
         <div>Gender: {submitted.gender}</div>
-        <div>Origin: {submitted.origin.name}</div>
-        <div>Origin Dimension: {submitted.origin.dimension}</div>
+        <div>Origin: {submitted?.origin?.name}</div>
+        <div>Origin Dimension: {submitted?.origin?.dimension}</div>
     </div>
     )}
     </form>
